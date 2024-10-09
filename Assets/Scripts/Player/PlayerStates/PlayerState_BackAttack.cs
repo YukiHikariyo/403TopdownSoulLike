cd32ee7250efde6daac932799d0ec5dba52b26dc
thenobody996 <411021455@qq.com>
2024-10-09 18:48:22 +0800

    见切和反击，暂未添加击中判定

--- Assets/Scripts/Player/PlayerStates/PlayerState_BackAttack.cs
+++ Assets/Scripts/Player/PlayerStates/PlayerState_BackAttack.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(menuName ="Data/PlayerState/BackAttack" ,fileName ="PlayerState_BackAttack")]
+[CreateAssetMenu(menuName = "Data/PlayerState/BackAttack", fileName = "PlayerState_BackAttack")]
 public class PlayerState_BackAttack : PlayerState
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Enter()
     {
+        base.Enter();
+        playerStateMachine.CanAcceptInput = false;
+        playerStateMachine.CanStateSwitch = false;
+        playerAnimator.Play("BackAttack");
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Exit()
     {
-        
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (playerStateMachine.CanAcceptInput)
+        {
+            if (playerInput.LightAttack)
+                playerStateMachine.memory = InputMemory.LightAttack;
+            else if (playerInput.Roll)
+                playerStateMachine.memory = InputMemory.Roll;
+        }
+        if (playerStateMachine.CanStateSwitch)
+        {
+            switch (playerStateMachine.memory)
+            {
+                case InputMemory.LightAttack:
+                    //攻击命中判定
+                    playerStateMachine.SwitchState(typeof(PlayerState_LightAttack_4));
+                    break;
+                case InputMemory.Roll:
+                    playerStateMachine.SwitchState(typeof(PlayerState_FirstRoll));
+                    break;
+            }
+        }
+        if (IsAnimationEnd)
+        {
+            //切换至移动
+            if (playerInput.WantsMove)
+            {
+                playerStateMachine.SwitchState(typeof(PlayerState_Move));
+            }
+            //切换至常态
+            if (!playerInput.WantsMove)
+            {
+                playerStateMachine.SwitchState(typeof(PlayerState_Idle));
+            }
+        }
+    }
+
+    public override void PhysicUpdate()
+    {
+        base.PhysicUpdate();
+        playerController.SkillDisplace(Skill_Physics.BackAttack, FaceDir, StateDuration / AnimationLength);
     }
 }
