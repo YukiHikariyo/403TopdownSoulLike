48ea69ac305a62e3428897bf43a18b441324037b
thenobody996 <411021455@qq.com>
2024-10-12 11:45:46 +0800

    唉子弹

---
+++ Assets/Scripts/Player/PlayerShooter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    [Header("子弹")]
+    [Tooltip("燃烧瓶")] public GameObject MolotovBottle;
+    [Tooltip("炫目光束")] public GameObject LightBullet;
+}
