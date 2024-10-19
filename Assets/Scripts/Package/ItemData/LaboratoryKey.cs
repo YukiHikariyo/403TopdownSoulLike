c687a8750da3d3879ebdbdab62d5550fc445cbb1
Yuki Hikariyo <3109209420@qq.com>
2024-10-19 11:59:56 +0800

    实验室钥匙

---
+++ Assets/Scripts/Package/ItemData/LaboratoryKey.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LaboratoryKey", menuName = "Data/Item/LaboratoryKey", order = 3)]
+public class LaboratoryKey : StaticItemData
+{
+    
+}
