e91025d9dfdeec3b00d92360433f7069124e7020
Yuki Hikariyo <3109209420@qq.com>
2024-10-24 19:05:33 +0800

    音量调节

--- Assets/Scripts/Tools/AudioManager.cs
+++ Assets/Scripts/Tools/AudioManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoSingleton<AudioManager>
 {
+    public AudioMixer audioMixer;
+    public Slider mainSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
     public SFXPool sfxPool;
 
     public AudioClip testClip;
@@ -16,4 +23,19 @@ public class AudioManager : MonoSingleton<AudioManager>
 
     [ContextMenu("播放测试音效")]
     public void TestPlaySFX() => PlaySFX(testClip, Vector3.zero);
+
+    public void OnMainVolumeChange(GameObject image)
+    {
+        audioMixer.SetFloat("MainVolume", 5 + 25 * Mathf.Log10(mainSlider.value > 0 ? mainSlider.value : 0.0001f));
+    }
+
+    public void OnMusicVolumeChange(GameObject image)
+    {
+        audioMixer.SetFloat("MusicVolume", 5 + 25 * Mathf.Log10(musicSlider.value > 0 ? musicSlider.value : 0.0001f));
+    }
+
+    public void OnSFXVolumeChange(GameObject image)
+    {
+        audioMixer.SetFloat("SFXVolume", 5 + 25 * Mathf.Log10(sfxSlider.value > 0 ? sfxSlider.value : 0.0001f));
+    }
 }
